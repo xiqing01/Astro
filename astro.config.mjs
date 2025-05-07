2b7e1a3f251150495520da01d734f514880a506a
xiqing <qingdiyu1@gmail.com>
2025-05-07 17:48:53 +0800

    完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
+
   vite: {
     plugins: [tailwindcss()]
   },
